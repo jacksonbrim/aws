3ae5c1fe322b289651c2063db5909211c1bc8bc1
Jackson Brim <jackson.cr.brim@gmail.com>
2022-09-23 01:23:10 -0500

    add pipeline stack, change app entry point to the pipeline stack, add codecommit repo to the pipeline stack

--- cdk_workshop/app.py
+++ cdk_workshop/app.py
@@ -2,10 +2,9 @@
 
 import aws_cdk as cdk
 
-from cdk_workshop.cdk_workshop_stack import CdkWorkshopStack
-
+from cdk_workshop.pipeline_stack import WorkShopPipelineStack
 
 app = cdk.App()
-CdkWorkshopStack(app, "cdk-workshop")
+WorkShopPipelineStack(app, "WorkShopPipelineStack")
 
 app.synth()
